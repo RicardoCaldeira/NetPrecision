43b592933069e916bccb1cada4841f8e1f4ec1db
Ricardo Caldeira <C0648888@vale.com>
2023-04-25 12:46:38 -0300

    remoção de código não necessário

--- lanchonete-api/src/main/java/com/netprecision/lanchonete/api/repository/PedidoRepository.java
+++ lanchonete-api/src/main/java/com/netprecision/lanchonete/api/repository/PedidoRepository.java
@@ -6,7 +6,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
-
-  List<Pedido> findAll();
-
 }
